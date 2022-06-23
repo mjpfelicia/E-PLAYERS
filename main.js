5eaa227da42cc08c7191de1561c23954693557bc
sandra <mjfelici@gmail.com>
2022-06-23 11:17:39 -0300

    add script no menu

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// 
+
+    const click = document.querySelector(".navbar-toggler");
+
+    click.addEventListener("click", function () {
+        const mostra = document.querySelector(".menu-responsivo");
+        if (mostra.style.display === "none")
+            mostra.style.display = "flex";
+        else {
+            mostra.style.display = "none";
+        }
+    })
+
